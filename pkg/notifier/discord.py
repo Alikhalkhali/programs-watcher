4b7c2cccdf54727d2c22b0434c7cb148b550211d
Alikhalkhali <alikhalkhali.org@gmail.com>
2023-04-30 16:31:07 +0430

    added new_program_message

--- pkg/notifier/discord.py
+++ pkg/notifier/discord.py
@@ -1,5 +1,38 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
+
+def new_program_message(data):
+    embed = DiscordEmbed(title=f"{data['programName']}",
+                         description=f"** New program ** named** {data['programName']} ** added to platform!\n** Program type: ** {data['programType']}\n\n** Program page: ** [Click here]({data['programURL']})", color=data['color'])
+    embed.set_thumbnail(url=data['logo'])
+    embed.set_footer(text='Powered by Ali Khalkhali',
+                     icon_url='https://cdn.discordapp.com/avatars/941457826662985808/488f3bcab0de041de57860b4e05e2e9f.webp')
+
+    if data["platformName"] in ["HackerOne", "Intigriti"]:
+        if data["platformName"] == "Intigriti":
+            if data['newReward']:
+                embed.add_embed_field(
+                    name="Bounty Table:", value=f"```{data['newReward']['min']} -- {data['newReward']['max']}```", inline=False)
+        if data["newScope"]:
+            scope = ""
+            for newScope in data["newScope"]:
+                scope += f"{shorten_string(newScope)}\n"
+            add_field(embed, scope, "Scope:")
+    if data["platformName"] in ["Bugcrowd", "YesWeHack"]:
+        if data['reward']:
+            embed.add_embed_field(
+                name="Bounty Table:", value=f"```{data['reward']['min']} -- {data['reward']['max']}```", inline=False)
+        if data['newInScope']:
+            inscope = '\n'.join(data['newInScope'])
+            add_field(embed, inscope, "InScope:")
+        if data["platformName"] == "Bugcrowd":
+            if data['newOutOfScope']:
+                newOutOfScope = '\n'.join(data['newOutOfScope'])
+                add_field(embed, newOutOfScope, "Out of scope:")
+    return embed
+
+
+
 def send_notification(data, webhook_url):
 
     webhook = DiscordWebhook(
