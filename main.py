f463558bc82f6dd8064233736043535e1885e1ea
Alikhalkhali <alikhalkhali.org@gmail.com>
2023-04-30 15:41:14 +0430

    connect to MongoDB

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import yaml
-
+from pymongo import MongoClient
 
 # load config file
 with open("config.yml", "r") as ymlfile:
@@ -11,3 +11,11 @@ for platform in cfg['platforms']:
         'url': platform['url'],
         'notifications': platform['notifications']
     }
+
+# connect to MongoDB
+client = MongoClient(cfg['mongoDB']['uri'])
+dbName = cfg['mongoDB']['database']
+db = client[dbName]
+first_time = False
+if dbName not in client.list_database_names():
+    first_time = True
