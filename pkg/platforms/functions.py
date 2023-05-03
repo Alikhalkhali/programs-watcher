206abbffe54f725ada9bcce275ae5df1fb52d789
Alikhalkhali <alikhalkhali.org@gmail.com>
2023-05-03 17:43:00 +0430

    added get_resource function

--- pkg/platforms/functions.py
+++ pkg/platforms/functions.py
@@ -1,4 +1,10 @@
+import requests
 from hashlib import md5
+
+
+def get_resource(tmp_dir, url, platformName):
+    domainList = requests.get(url, allow_redirects=True)
+    open(F"{tmp_dir}{platformName}.json", 'wb').write(domainList.content)
 
 
 def generate_program_key(programName, programURL):
