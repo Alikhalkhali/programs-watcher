de9511915f7d7ba7baa6970c7d38d61e69189328
Alikhalkhali <alikhalkhali.org@gmail.com>
2023-05-03 17:57:53 +0430

    call checking functions

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import yaml
 from pymongo import MongoClient
 import os
 import shutil
+from pkg.platforms.bugcrowd import check_bugcrowd
+from pkg.platforms.hackerone import check_hackerone
+from pkg.platforms.intigriti import check_intigriti
+from pkg.platforms.yeswehack import check_yeswehack
+
+
 # load config file
 with open("config.yml", "r") as ymlfile:
     cfg = yaml.full_load(ymlfile)
@@ -29,6 +35,17 @@ if os.path.exists(tmp_dir):
 else:
     os.mkdir(tmp_dir)
 
+# checking bugcrowd
+check_bugcrowd(tmp_dir, mUrl, first_time, db, platforms['bugcrowd'])
+
+# checking hackerone
+check_hackerone(tmp_dir, mUrl, first_time, db, platforms['hackerone'])
+
+# checking intigriti
+check_intigriti(tmp_dir, mUrl, first_time, db, platforms['intigriti'])
+
+# checking yeswehack
+check_yeswehack(tmp_dir, mUrl, first_time, db, platforms['yeswehack'])
 
 # Clean up resources and remove tmp_dir
 client.close()
